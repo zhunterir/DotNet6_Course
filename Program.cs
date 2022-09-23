c5a9a6363d495b472c799eac86bb0525d7b0ea20
MohammadMN <mmn137555@gmail.com>
2022-09-23 07:54:44 +0330

    Code for Using The IHostApplicationLifetime for customizing Application Started, Apllication Stopping and Application Stopped event handling was added .

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 using DotNet6_Course;
 using DotNet6_Course.MyServices;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IConsoleLogSerivce, MyConsoleLogService>();
 builder.Services.AddTransient<SampleFactoryBasedMiddleware>();
 var app = builder.Build();
+IHostApplicationLifetime lifeTime = app.Lifetime;
+var myHostConfi = new MyHostConfiguation(lifeTime) ;
+myHostConfi.ConfigureServer();
 app.Map("/Branch1", BranchOne); // for branching when request path starts with specific part
 app.MapWhen(context => context.Request.Query.ContainsKey("Name"),
     appBuilder => NameBranch(appBuilder));
@@ -36,4 +41,29 @@ static void Branch3(IApplicationBuilder app)
         await context.Response.WriteAsync("From Br3");
     });
 
+}
+
+public class MyHostConfiguation
+{
+    private readonly IHostApplicationLifetime lifetime;
+
+    public MyHostConfiguation(IHostApplicationLifetime lifetime)
+    {
+        this.lifetime = lifetime;
+    }
+    public void ConfigureServer()
+    {
+        lifetime.ApplicationStarted.Register(() =>
+        {
+            Debug.WriteLine("Application was started hoooraaa. (just a life time test)");
+        });
+        lifetime.ApplicationStopping.Register(() =>
+        {
+            Debug.WriteLine("Apllicatiion is stopping ( just a life time test)");
+        });
+        lifetime.ApplicationStopped.Register(() =>
+        {
+            Debug.WriteLine("Apllicatiion was stopped ( just a life time test)");
+        });
+    }
 }
